717ddb59e00ba697c288af8d83681fcf2e6b1b2a
Junlapong L <junlapong@gmail.com>
2016-06-20 10:42:41 +0700

    add new-epay demo new design

---
+++ new-epay/js/main.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+    //Initialize tooltips
+    $('.nav-tabs > li a[title]').tooltip();
+    
+    //Wizard
+    $('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
+
+        var $target = $(e.target);
+    
+        if ($target.parent().hasClass('disabled')) {
+            return false;
+        }
+    });
+
+    $(".next-step").click(function (e) {
+
+        var $active = $('.wizard .nav-tabs li.active');
+        $active.next().removeClass('disabled');
+        nextTab($active);
+
+    });
+    $(".prev-step").click(function (e) {
+
+        var $active = $('.wizard .nav-tabs li.active');
+        prevTab($active);
+
+    });
+});
+
+function nextTab(elem) {
+    $(elem).next().find('a[data-toggle="tab"]').click();
+}
+function prevTab(elem) {
+    $(elem).prev().find('a[data-toggle="tab"]').click();
+}// JavaScript Document
+
+(function($) {
+		"use strict";
+		
+		$("h1").fitText(
+						1.2, {
+								minFontSize: '35px',
+								maxFontSize: '65px'
+						}
+				);
+})(jQuery);		
