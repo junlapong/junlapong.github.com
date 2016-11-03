e8e8efebdbf792b296777409ac65723e49166685
Junlapong L <junlapong@gmail.com>
2016-11-03 18:00:39 +0700

    update design

--- new-epay/js/main.js
+++ new-epay/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
-    //Initialize tooltips
-    $('.nav-tabs > li a[title]').tooltip();
     
+		"use strict";
+        
     //Wizard
     $('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
 
@@ -12,14 +12,14 @@ $(document).ready(function () {
         }
     });
 
-    $(".next-step").click(function (e) {
+    $(".next-step").click(function () {
 
         var $active = $('.wizard .nav-tabs li.active');
         $active.next().removeClass('disabled');
         nextTab($active);
 
     });
-    $(".prev-step").click(function (e) {
+    $(".prev-step").click(function () {
 
         var $active = $('.wizard .nav-tabs li.active');
         prevTab($active);
@@ -27,20 +27,51 @@ $(document).ready(function () {
     });
 });
 
+
 function nextTab(elem) {
+		"use strict";		
     $(elem).next().find('a[data-toggle="tab"]').click();
 }
 function prevTab(elem) {
+		"use strict";		
     $(elem).prev().find('a[data-toggle="tab"]').click();
 }// JavaScript Document
 
 (function($) {
 		"use strict";
 		
-		$("h1").fitText(
+		//Initialize tooltips
+		$('.nav-tabs > li a[title]').tooltip();
+		
+		$(".topbar > h3").fitText(
 						1.2, {
-								minFontSize: '35px',
-								maxFontSize: '65px'
+								minFontSize: '14px',
+								maxFontSize: '18px'
 						}
-				);
+		);
+		
+		$("h1, .signin").fitText(
+						1.2, {
+								minFontSize: '16px',
+								maxFontSize: '27px'
+						}
+		);
+
+		$(".box-login h5").fitText(
+										1.2, {
+														minFontSize: '11px',
+														maxFontSize: '14px'
+										}
+		);
+		
+		$(".txt-retail h5").fitText(
+										1.2, {
+														minFontSize: '10px',
+														maxFontSize: '12px'
+										}
+		);
+				
+				
+				
+        
 })(jQuery);		
